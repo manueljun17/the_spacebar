1d83b7b63651b416e6551430c1725b19f5ff8ae4
Manuel Manansala <manuelmanansalajr@gmail.com>
2019-03-25 18:30:12 +0800

    Services

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use Psr\Log\LoggerInterface;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -36,9 +37,9 @@ class ArticleController extends AbstractController
     /**
      * @Route("/news/{slug}/heart", name="article_toggle_heart", methods={"POST"})
      */
-    public function toggleArticleHeart($slug) {
+    public function toggleArticleHeart($slug, LoggerInterface $logger) {
         //TODO - actually heart/unheart the article!
-
+        $logger->info("Article is being hearted");
         return $this->json(['hearts' => rand(5, 100)]);
     }
 }
